5620fa6f4e7c64a4a50d41e4b9ba81f97fef6c37
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-08-27 16:00:08 +0600

    Create 1480. Running Sum of 1d Array.cpp

---
+++ Algorithms/1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> vec;
+        int sum = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            vec.push_back(sum);
+        }
+        
+        return vec;
+    }
+};
