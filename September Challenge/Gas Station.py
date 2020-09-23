d61a1f5e928140eea4252120fe13f51db7217194
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-09-23 16:40:39 +0600

    Create Gas Station.py

---
+++ September Challenge/Gas Station.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        n = len(gas)
+        start = 0
+        tank = 0
+        
+        for i in range(n):
+            tank += gas[i] - cost[i]
+            
+            if tank < 0:
+                tank = 0
+                start = i + 1
+        
+        return start
+            
