e035a3c2a94f2edb8c4c1523b6e0dd518f524520
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-10-08 13:45:50 +0600

    Create Binary Search.py

---
+++ October Challenge/Binary Search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) - 1
+      
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        
+        return -1
+        
