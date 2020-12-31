0578ddd238ac23fa49fa514d864e15666b5f0efc
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-12-31 18:52:03 +0600

    Create Largest Rectangle in Histogram.cpp

---
+++ December Challenge/Largest Rectangle in Histogram.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> stk;
+
+        int i = 0, j;
+        int n = heights.size();
+
+        int ans = 0;
+        while(i < n)
+        {
+            if(stk.empty() || heights[stk.top()] <= heights[i])
+                stk.push(i), i++;
+
+            else
+            {
+                j = stk.top();
+                stk.pop();
+
+                // empty means all those before this one is greater than this one.
+                if(stk.empty())
+                    ans = max(ans, heights[j] * i);
+
+                // length from te popped element to i
+                else
+                    ans = max(ans, heights[j] * (i - stk.top() - 1));
+            }
+        }
+
+        while(!stk.empty())
+        {
+            j = stk.top();
+            stk.pop();
+
+            // empty means all those before this one is greater than this one.
+            if(stk.empty())
+                ans = max(ans, heights[j] * i);
+
+            // length from te popped element to i
+            else
+                ans = max(ans, heights[j] * (i - stk.top() - 1));
+        }
+
+        return ans;
+    }
+};
