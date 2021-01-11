d5b52e63bec0edb546fdb1acc974f553fd532835
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-01-11 20:26:18 +0600

    Create Merge Sorted Array.py

---
+++ January, 2021 Challenge/Merge Sorted Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        j = 0
+        for i in range(m, m + n):
+            nums1[i] = nums2[j]
+            j += 1
+        
+        nums1.sort()
