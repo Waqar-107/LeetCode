71fca46b071ceb7a12af59d18ce2d546e7f1cbe1
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-02-12 16:32:20 +0600

    Create Number of Steps to Reduce a Number to Zero.py

---
+++ February, 2021 Challenge/Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        ans = 0
+        while num:
+            if num % 2:
+                num -= 1
+                ans += 1
+            
+            if num:
+                num //= 2
+                ans += 1
+        
+        return ans
+            
