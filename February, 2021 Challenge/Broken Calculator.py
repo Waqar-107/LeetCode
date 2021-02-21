711d8fbaac0b4959b57e32c50d83485de526d2b7
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-02-21 15:46:49 +0600

    Create Broken Calculator.py

---
+++ February, 2021 Challenge/Broken Calculator.py
@@ -0,0 +1,12 @@
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int:
+        ans = 0
+        while Y > X:
+            if Y % 2:
+                Y += 1
+            else:
+                Y //= 2
+            
+            ans += 1
+        
+        return ans + X - Y
