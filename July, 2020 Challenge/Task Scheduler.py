432f14c6d193168fb4bb9f8f91e4b6b7f642dee2
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-03-31 04:29:21 +0600

    Create Task Scheduler.py

---
+++ July, 2020 Challenge/Task Scheduler.py
@@ -0,0 +1,19 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        cnt = [0] * 26
+        length = len(tasks)
+
+        for ch in tasks:
+            cnt[ord(ch) - ord('A')] += 1
+            
+        cnt.sort()
+        
+        mx = cnt.pop()
+        idle = (mx - 1) * n
+        
+        while len(cnt) and idle > 0:
+            idle -= min(mx - 1, cnt.pop())
+        
+        return idle + len(tasks)
+        
+        
