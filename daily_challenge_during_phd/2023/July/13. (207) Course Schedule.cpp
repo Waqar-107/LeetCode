8a65c76e15cf9ce86f34db68badf9a978c012d1f
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2023-07-13 17:07:42 -0700

    Create 13. (207) Course Schedule.cpp

---
+++ daily_challenge_during_phd/2023/July/13. (207) Course Schedule.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> adj;
+    vector<int> vis;
+    bool cycle;
+
+    void dfs(int src)
+    {
+        if(!vis[src])
+        {
+            vis[src] = 1;
+
+            for(int i = 0; i < adj[src].size(); i++)
+                dfs(adj[src][i]);
+
+            vis[src] = 2;
+        }
+
+        else if(vis[src] == 1)
+            cycle = true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        adj.resize(numCourses);
+        vis.resize(numCourses);
+
+        fill(vis.begin(), vis.end(), 0);
+        cycle = false;
+
+        for(int i = 0; i < prerequisites.size(); i++)
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(!vis[i])
+                dfs(i);
+        }
+
+        bool ans = true & !cycle;
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(!vis[i])
+            {
+                ans = false;
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
